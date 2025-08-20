2b26209b28e0c7b86c71a716c5ba5be255ef5007
Nia <niaespinal@outlook.com>
2025-08-20 22:29:15 +0200

    Initialize project

---
+++ src/main/java/com/FemCoders/DropTheLink/DropTheLinkApplication.java
@@ -0,0 +1,13 @@
+package com.FemCoders.DropTheLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropTheLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropTheLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FemCoders/DropTheLink/DropTheLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FemCoders.DropTheLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropTheLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
